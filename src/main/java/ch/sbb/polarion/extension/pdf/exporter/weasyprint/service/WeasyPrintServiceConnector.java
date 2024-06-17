75f31fdd61454db297165e1b04837669525785e5
nirikash <141118539+nirikash@users.noreply.github.com>
2024-06-17 11:21:02 +0200

    Feature/weasyprint version logging (#18)
    * feat: Logging Weasyprint service versions
    
    Refs: #DEV-11775

--- src/main/java/ch/sbb/polarion/extension/pdf/exporter/weasyprint/service/WeasyPrintServiceConnector.java
+++ src/main/java/ch/sbb/polarion/extension/pdf/exporter/weasyprint/service/WeasyPrintServiceConnector.java
@@ -6,6 +6,7 @@ import ch.sbb.polarion.extension.pdf.exporter.weasyprint.WeasyPrintOptions;
 import ch.sbb.polarion.extension.pdf.exporter.weasyprint.service.model.WeasyPrintServiceVersion;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.polarion.core.util.logging.Logger;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -16,8 +17,16 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.module.ModuleDescriptor;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.polarion.core.util.StringUtils.isEmpty;
 
 public class WeasyPrintServiceConnector implements WeasyPrintConverter {
+    private static final Logger logger = Logger.getLogger(WeasyPrintServiceConnector.class);
+    private static final String WEASYPRINT_VERSION_HEADER = "Weasyprint-Version";
+    private static final String PYTHON_VERSION_HEADER = "Python-Version";
+    private static final AtomicReference<String> weasyPrintVersion = new AtomicReference<>();
+    private static final AtomicReference<String> pythonVersion = new AtomicReference<>();
 
     @Override
     public byte[] convertToPdf(String htmlPage, WeasyPrintOptions weasyPrintOptions) {
@@ -36,6 +45,7 @@ public class WeasyPrintServiceConnector implements WeasyPrintConverter {
                 if (response.getStatus() == Response.Status.OK.getStatusCode()) {
                     InputStream inputStream = response.readEntity(InputStream.class);
                     try {
+                        logWeasyPrintVersionFromHeader(response);
                         return inputStream.readAllBytes();
                     } catch (IOException e) {
                         throw new IllegalStateException("Could not read response stream", e);
@@ -83,4 +93,20 @@ public class WeasyPrintServiceConnector implements WeasyPrintConverter {
         return PdfExporterExtensionConfiguration.getInstance().getWeasyprintService();
     }
 
+    private void logWeasyPrintVersionFromHeader(Response response) {
+        String actualWeasyPrintVersion = response.getHeaderString(WEASYPRINT_VERSION_HEADER);
+        String actualPythonVersion = response.getHeaderString(PYTHON_VERSION_HEADER);
+
+        boolean hasWeasyPrintVersionChanged = hasVersionChanged(actualWeasyPrintVersion, weasyPrintVersion);
+        boolean hasPythonVersionChanged = hasVersionChanged(actualPythonVersion, pythonVersion);
+
+        if (hasWeasyPrintVersionChanged || hasPythonVersionChanged) {
+            logger.info(String.format("WeasyPrintService uses WeasyPrint version '%s' and Python version '%s'", actualWeasyPrintVersion, actualPythonVersion));
+        }
+    }
+
+    public boolean hasVersionChanged(String actualVersion, AtomicReference<String> version) {
+        return !isEmpty(actualVersion)
+                && !actualVersion.equals(version.getAndSet(actualVersion));
+    }
 }
