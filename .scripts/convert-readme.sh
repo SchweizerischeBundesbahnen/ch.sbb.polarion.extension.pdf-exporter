4c9f00f0948bad5169081cafd4a96fab6251ba5e
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-06-24 18:36:59 +0200

    feat: content of about page now generated based on information from README.md

--- .scripts/convert-readme.sh
+++ .scripts/convert-readme.sh
@@ -3,13 +3,26 @@
 INPUT_FILE=$1
 OUTPUT_FILE=$2
 
+INPUT_FILE="${INPUT_FILE:-README.md}"
+OUTPUT_FILE="${OUTPUT_FILE:-README.html}"
+
 # Convert the markdown file to a JSON payload
 jq -R -s '{"mode": "gfm", "text": .}' < "$INPUT_FILE" > payload.json
+
+echo "GITHUB_TOKEN = $GITHUB_TOKEN"
+
+# Check if GITHUB_TOKEN is set
+if [[ -n "$GITHUB_TOKEN" ]]; then
+  AUTH_HEADER="Authorization: Bearer $GITHUB_TOKEN"
+else
+  AUTH_HEADER=""
+fi
 
 # Send the JSON payload to the GitHub API
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
+  ${AUTH_HEADER:+-H "$AUTH_HEADER"} \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/markdown \
   -d @payload.json > "$OUTPUT_FILE"
