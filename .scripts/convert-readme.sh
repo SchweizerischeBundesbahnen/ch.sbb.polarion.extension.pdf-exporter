32add8980d533d20b6e0829d1ddb9b129f82818e
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-06-27 14:07:37 +0200

    fix: check if required commands are available
    maven build will not fail on local machines, but will fail during GitHub Actions build

--- .scripts/convert-readme.sh
+++ .scripts/convert-readme.sh
@@ -7,12 +7,17 @@ function check_command() {
   if ! command -v "$1" &> /dev/null; then
     echo -e "\033[0;31m $1 is not installed! \033[0m" >&2
     echo -e "\033[0;31m Help for About page will not be generated! \033[0m" >&2
-    exit 0
+
+    if [ -n "$FAIL_ON_CHECK_COMMANDS" ]; then
+      exit 1
+    else
+      exit 0
+    fi
   fi
 }
 
 # Check if required commands are installed
-check_command jq
+check_command jq1
 check_command curl
 check_command awk
 
