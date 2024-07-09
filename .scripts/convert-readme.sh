45cf6fef038f7065051a2b3a40232676c3d45688
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-07-09 14:21:23 +0200

    fix: HTTP Status 500 in About page by migration to generic v6.1.0 (#73)
    convert-readme.sh updated to use github token
    
    Refs: #71

--- .scripts/convert-readme.sh
+++ .scripts/convert-readme.sh
@@ -20,6 +20,8 @@ function check_command() {
 check_command jq
 check_command curl
 check_command awk
+check_command sed
+check_command tr
 
 INPUT_FILE="${1:-README.md}"
 OUTPUT_FILE="${2:-README.html}"
@@ -27,16 +29,35 @@ OUTPUT_FILE="${2:-README.html}"
 # Convert the markdown file to a JSON payload
 JSON_PAYLOAD=$(jq -R -s '{"mode": "gfm", "text": .}' < "$INPUT_FILE")
 
-# Send the JSON payload to the GitHub API
-CURL_OUTPUT=$(curl -L \
+# Check if GITHUB_TOKEN is set
+if [[ -n "$GITHUB_TOKEN" ]]; then
+  AUTH_HEADER="Authorization: Bearer $GITHUB_TOKEN"
+else
+  AUTH_HEADER=""
+fi
+
+# Send the JSON payload to the GitHub API and capture the HTTP response code and body
+HTTP_RESPONSE=$(curl -s -w "HTTPSTATUS:%{http_code}" -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
+  ${AUTH_HEADER:+-H "$AUTH_HEADER"} \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/markdown \
   -d "$JSON_PAYLOAD")
 
+# Extract the status and the body from the HTTP response
+HTTP_BODY=$(echo "$HTTP_RESPONSE" | sed -e 's/HTTPSTATUS:.*//g')
+HTTP_STATUS=$(echo "$HTTP_RESPONSE" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+# Check the HTTP status
+if [ "$HTTP_STATUS" -ne 200 ]; then
+  echo -e "\033[0;31m README.md: HTTP request failed with status $HTTP_STATUS. Response: \033[0m" >&2
+  echo -e "\033[0;31m $HTTP_BODY \033[0m" >&2
+  exit 1
+fi
+
 # Process the curl output with awk to remove the Build, Installation and Changelog sections
-MODIFIED_CONTENT=$(echo "$CURL_OUTPUT" | awk '
+MODIFIED_CONTENT=$(echo "$HTTP_BODY" | awk '
 /<h2>Build<\/h2>/ {skip=1; next}
 /<h2>Polarion configuration<\/h2>/ {skip=0}
 /<h2>Changelog<\/h2>/ {skip=1; next}
@@ -44,3 +65,5 @@ MODIFIED_CONTENT=$(echo "$CURL_OUTPUT" | awk '
 
 # Write the modified content to the output file
 echo "$MODIFIED_CONTENT" > "$OUTPUT_FILE"
+
+echo "README.md: HTTP request successful. Output saved to $OUTPUT_FILE."
