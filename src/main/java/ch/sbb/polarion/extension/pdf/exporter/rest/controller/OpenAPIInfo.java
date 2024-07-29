3c49465574f5dd4c6796578b64c2457505068d12
e550448 <andrei.shakirin@sbb.ch>
2024-07-29 14:11:48 +0200

    docs: documentation added, OpenAPI Spec generated
    Refs: #97

---
+++ src/main/java/ch/sbb/polarion/extension/pdf/exporter/rest/controller/OpenAPIInfo.java
@@ -0,0 +1,17 @@
+package ch.sbb.polarion.extension.pdf.exporter.rest.controller;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "PDF Exporter REST API",
+                version = "v1",
+                description = "",
+                termsOfService = "",
+                contact = @Contact(name = "", url = ""),
+                license = @License(name = "", url = "")))
+public class OpenAPIInfo {
+}
