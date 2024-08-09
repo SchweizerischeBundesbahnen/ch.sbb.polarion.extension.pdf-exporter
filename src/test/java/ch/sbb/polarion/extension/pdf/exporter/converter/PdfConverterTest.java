31fd642549d18ff8170e33e2398719f9147ffb17
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-08-09 18:29:25 +0200

    fix: LiveReport Page now respects URL Parameters (#150)
    Refs: #147

--- src/test/java/ch/sbb/polarion/extension/pdf/exporter/converter/PdfConverterTest.java
+++ src/test/java/ch/sbb/polarion/extension/pdf/exporter/converter/PdfConverterTest.java
@@ -83,7 +83,7 @@ class PdfConverterTest {
                 .builder()
                 .documentTitle("testDocument")
                 .documentContent("test document content").build();
-        when(liveDocHelper.getLiveDocument(project, exportParams, true)).thenReturn(documentData);
+        when(liveDocHelper.getLiveDocument(project, exportParams)).thenReturn(documentData);
         when(headerFooterSettings.load("test project", SettingId.fromName("Default"))).thenReturn(HeaderFooterModel.builder().build());
         when(placeholderProcessor.replacePlaceholders(documentData, exportParams, "test css")).thenReturn("css content");
         when(placeholderProcessor.replacePlaceholders(eq(documentData), eq(exportParams), anyList())).thenReturn(List.of("hl", "hc", "hr", "fl", "fc", "fr"));
