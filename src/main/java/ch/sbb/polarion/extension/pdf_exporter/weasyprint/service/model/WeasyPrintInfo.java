edb4014bd54f2a913847efc874be16ed288b72fe
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-09-23 15:00:17 +0200

    feat: display weasyprint server build timestamp and chromium version in extension info table
    Refs: #218

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/weasyprint/service/model/WeasyPrintInfo.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/weasyprint/service/model/WeasyPrintInfo.java
@@ -1,11 +1,15 @@
 package ch.sbb.polarion.extension.pdf_exporter.weasyprint.service.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.jetbrains.annotations.Nullable;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeasyPrintInfo {
+    private @Nullable String chromium;
     private @Nullable String python;
+    private @Nullable String timestamp;
     private @Nullable String weasyprint;
     private @Nullable String weasyprintService;
 }
