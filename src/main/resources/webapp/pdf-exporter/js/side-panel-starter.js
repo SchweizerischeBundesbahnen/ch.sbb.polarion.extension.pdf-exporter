c09cac1f9cd09148b937e46180930213d8ab8a70
Dzmitry Yurtsevich <dzmitry.yurtsevich@sbb.ch>
2024-09-24 15:34:57 +0200

    fix: Fix loading JS module (#225)
    Refs: #222

--- src/main/resources/webapp/pdf-exporter/js/side-panel-starter.js
+++ src/main/resources/webapp/pdf-exporter/js/side-panel-starter.js
@@ -3,17 +3,18 @@ const PdfExporterSidePanelStarter = {
 
     injectAll: function () {
         this.injectScript("common-script", `/polarion/pdf-exporter/ui/generic/js/common.js${this.timestampParam}`);
-        this.injectScript("ExportParams-script", `/polarion/pdf-exporter/ui/js/ExportParams.js${this.timestampParam}`);
+        this.injectScript("ExportParams-script", `/polarion/pdf-exporter/ui/js/modules/ExportParams.js${this.timestampParam}`, 'module');
         this.injectScript("export-common-script", `/polarion/pdf-exporter/ui/js/export-common.js${this.timestampParam}`);
         this.injectScript("export-pdf-script", `/polarion/pdf-exporter/ui/js/export-pdf.js${this.timestampParam}`);
     },
 
 
-    injectScript: function (id, componentScriptPath) {
+    injectScript: function (id, componentScriptPath, type = "text/javascript") {
         if (top.document.body && !top.document.getElementById(id)) {
             const scriptElement = document.createElement("script");
             scriptElement.id = id;
             scriptElement.setAttribute("src", componentScriptPath);
+            scriptElement.setAttribute("type", type);
             top.document.body.appendChild(scriptElement);
         }
     },
