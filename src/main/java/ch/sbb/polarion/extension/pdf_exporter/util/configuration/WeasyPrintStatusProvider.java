1bf4bc5e010101b4ec60d3da9a90a3b147f69abe
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-10-21 11:06:21 +0200

    feat: detect whether upgrade of WeasyPrint Service is required and display it in About page
    Refs: #258

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/configuration/WeasyPrintStatusProvider.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/configuration/WeasyPrintStatusProvider.java
@@ -12,8 +12,11 @@ import ch.sbb.polarion.extension.pdf_exporter.weasyprint.service.model.WeasyPrin
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 @Discoverable
 public class WeasyPrintStatusProvider extends ConfigurationStatusProvider {
@@ -42,7 +45,7 @@ public class WeasyPrintStatusProvider extends ConfigurationStatusProvider {
             htmlToPdfConverter.convert("<html><body>test html</body></html>", Orientation.PORTRAIT, PaperSize.A4);
 
             return List.of(
-                    createWeasyPrintStatus(WEASY_PRINT_SERVICE_INFO.get(WeasyPrintServiceInfo.VERSION), weasyPrintInfo.getWeasyprintService() + " (" + weasyPrintInfo.getTimestamp() + ")"),
+                    createWeasyPrintStatus(WEASY_PRINT_SERVICE_INFO.get(WeasyPrintServiceInfo.VERSION), weasyPrintInfo.getWeasyprintService(), weasyPrintInfo.getTimestamp(), getLatestCompatibleVersionWeasyPrintService()),
                     createWeasyPrintStatus(WEASY_PRINT_SERVICE_INFO.get(WeasyPrintServiceInfo.PYTHON), weasyPrintInfo.getPython()),
                     createWeasyPrintStatus(WEASY_PRINT_SERVICE_INFO.get(WeasyPrintServiceInfo.WEASYPRINT), weasyPrintInfo.getWeasyprint()),
                     createWeasyPrintStatus(WEASY_PRINT_SERVICE_INFO.get(WeasyPrintServiceInfo.CHROMIUM), weasyPrintInfo.getChromium())
@@ -52,11 +55,48 @@ public class WeasyPrintStatusProvider extends ConfigurationStatusProvider {
         }
     }
 
-    private static @NotNull ConfigurationStatus createWeasyPrintStatus(@NotNull String name, @Nullable String description) {
-        if (description == null || description.isBlank()) {
-            return new ConfigurationStatus(name, Status.WARNING, "Unknown");
+    private static @NotNull ConfigurationStatus createWeasyPrintStatus(@NotNull String name, @Nullable String version) {
+        if (version == null || version.isBlank()) {
+            return new ConfigurationStatus(name, Status.ERROR, "Unknown");
         } else {
-            return new ConfigurationStatus(name, Status.OK, description);
+            return new ConfigurationStatus(name, Status.OK, version);
         }
     }
+
+    private static @NotNull ConfigurationStatus createWeasyPrintStatus(@NotNull String name, @Nullable String version, @Nullable String timestamp, @Nullable String latestCompatibleVersion) {
+        if (version == null || version.isBlank()) {
+            return new ConfigurationStatus(name, Status.ERROR, createUpgradeWeasyPrintMessage("Unknown", timestamp, latestCompatibleVersion));
+        } else if (!version.equals(latestCompatibleVersion)) {
+            return new ConfigurationStatus(name, Status.WARNING, createUpgradeWeasyPrintMessage(version, timestamp, latestCompatibleVersion));
+        } else {
+            return new ConfigurationStatus(name, Status.OK, version);
+        }
+    }
+
+    private static @NotNull String createUpgradeWeasyPrintMessage(@NotNull String version, @Nullable String timestamp, @Nullable String latestCompatibleVersion) {
+        StringBuilder message = new StringBuilder();
+        message.append(version);
+        if (timestamp != null && !timestamp.isBlank()) {
+            message.append(" (").append(timestamp).append(")");
+        }
+        if (latestCompatibleVersion != null && !latestCompatibleVersion.isBlank()) {
+            message.append(": <span style='color: red;'>upgrade to</span> <a href='https://github.com/SchweizerischeBundesbahnen/weasyprint-service/releases/tag/v").append(latestCompatibleVersion).append("' target='_blank'>").append(latestCompatibleVersion).append("</a>");
+        }
+        return message.toString();
+    }
+
+    static @Nullable String getLatestCompatibleVersionWeasyPrintService() {
+        try (InputStream input = WeasyPrintStatusProvider.class.getClassLoader().getResourceAsStream("versions.properties")) {
+            if (input == null) {
+                return null;
+            }
+
+            Properties properties = new Properties();
+            properties.load(input);
+            return properties.getProperty("weasyprint-service.version");
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
 }
