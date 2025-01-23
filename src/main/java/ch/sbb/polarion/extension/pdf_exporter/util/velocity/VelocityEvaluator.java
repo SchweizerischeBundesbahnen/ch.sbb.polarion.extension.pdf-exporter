a04f279216c98e2fa1aa448b5aa905adf96bbd8c
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-01-23 19:26:08 +0100

    feat: evaluate velocity using RichPageScriptRenderer
    Refs: #344

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/velocity/VelocityEvaluator.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/velocity/VelocityEvaluator.java
@@ -3,47 +3,31 @@ package ch.sbb.polarion.extension.pdf_exporter.util.velocity;
 import ch.sbb.polarion.extension.generic.util.ObjectUtils;
 import ch.sbb.polarion.extension.pdf_exporter.rest.model.documents.DocumentData;
 import com.polarion.alm.projects.model.IUniqueObject;
-import com.polarion.alm.shared.api.transaction.ReadOnlyTransaction;
+import com.polarion.alm.server.api.model.rp.widget.RichPageScriptRenderer;
+import com.polarion.alm.server.api.model.rp.widget.impl.RichPageRenderingContextImpl;
 import com.polarion.alm.shared.api.transaction.TransactionalExecutor;
-import com.polarion.alm.shared.util.VelocityContextInitializer;
+import com.polarion.alm.shared.api.transaction.internal.InternalReadOnlyTransaction;
 import com.polarion.alm.tracker.model.IModule;
 import com.polarion.alm.tracker.model.IRichPage;
 import com.polarion.alm.tracker.model.ITestRun;
 import com.polarion.alm.tracker.model.IWikiPage;
 import com.polarion.alm.tracker.model.baselinecollection.IBaselineCollection;
-import com.polarion.alm.ui.server.VelocityFactory;
-import com.polarion.core.util.logging.Logger;
 import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.exception.MethodInvocationException;
-import org.apache.velocity.exception.ParseErrorException;
-import org.apache.velocity.exception.ResourceNotFoundException;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.StringWriter;
 
 public class VelocityEvaluator {
 
-    private static final Logger log = Logger.getLogger(VelocityEvaluator.class);
-
     public @NotNull String evaluateVelocityExpressions(@NotNull DocumentData<? extends IUniqueObject> documentData, @NotNull String template) {
         return ObjectUtils.requireNotNull(TransactionalExecutor.executeSafelyInReadOnlyTransaction(transaction -> {
-            VelocityContext velocityContext = createVelocityContext(transaction, documentData);
-            StringWriter writer = new StringWriter();
-            VelocityEngine velocityEngine = new VelocityFactory().engine();
-
-            try {
-                velocityEngine.evaluate(velocityContext, writer, null, template);
-                return writer.toString();
-            } catch (ParseErrorException | MethodInvocationException | ResourceNotFoundException e) {
-                log.error("error during velocity template evaluation", e);
-                return template;
-            }
+            RichPageRenderingContextImpl richPageRenderingContext = new RichPageRenderingContextImpl((InternalReadOnlyTransaction) transaction);
+            RichPageScriptRenderer richPageScriptRenderer = new RichPageScriptRenderer(richPageRenderingContext, template, documentData.getId().getDocumentId());
+            VelocityContext velocityContext = richPageScriptRenderer.velocityContext();
+            updateVelocityContext(velocityContext, documentData);
+            return richPageScriptRenderer.renderHtml();
         }));
     }
 
-    private @NotNull VelocityContext createVelocityContext(@NotNull ReadOnlyTransaction transaction, @NotNull DocumentData<? extends IUniqueObject> documentData) {
-        VelocityContext velocityContext = new VelocityContextInitializer(transaction).create();
+    private void updateVelocityContext(@NotNull VelocityContext velocityContext, @NotNull DocumentData<? extends IUniqueObject> documentData) {
 
         switch (documentData.getType()) {
             case LIVE_DOC -> {
@@ -74,7 +58,5 @@ public class VelocityEvaluator {
         }
 
         velocityContext.put("projectName", documentData.getId().getDocumentProject() != null ? documentData.getId().getDocumentProject().getName() : "");
-
-        return velocityContext;
     }
 }
