3625856c621914b5bce9c60978af23d4553e4945
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-02-12 15:31:59 +0100

    fix: use Polarion API to validate scriptInjection
    Refs: #369

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/configuration/DleToolbarStatusProvider.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/configuration/DleToolbarStatusProvider.java
@@ -2,22 +2,29 @@ package ch.sbb.polarion.extension.pdf_exporter.util.configuration;
 
 import ch.sbb.polarion.extension.generic.configuration.ConfigurationStatus;
 import ch.sbb.polarion.extension.generic.configuration.ConfigurationStatusProvider;
-import ch.sbb.polarion.extension.generic.settings.SettingsService;
+import ch.sbb.polarion.extension.generic.configuration.Status;
 import ch.sbb.polarion.extension.generic.util.Discoverable;
-import ch.sbb.polarion.extension.generic.util.ScopeUtils;
-import com.polarion.subterra.base.location.ILocation;
+import com.polarion.alm.projects.properties.internal.ScriptInjectionPropertiesProvider;
 import org.jetbrains.annotations.NotNull;
 
 @Discoverable
 public class DleToolbarStatusProvider extends ConfigurationStatusProvider {
 
     public static final String DLE_TOOLBAR = "DLE Toolbar";
+    public static final String DLE_TOOLBAR_SCRIPT_REGEX = "(.*)<script src=\"/polarion/pdf-exporter/js/starter.js\"></script>(.*)<script>PdfExporterStarter.injectToolbar(.*);</script>(.*)";
 
     @Override
     public @NotNull ConfigurationStatus getStatus(@NotNull Context context) {
-        ILocation location = ScopeUtils.getDefaultLocation().append(".polarion/context.properties");
-        String content = new SettingsService().read(location, null);
+        String scriptInjectionSystemPropertiesDleEditorHead = ScriptInjectionPropertiesProvider.getScriptInjectionSystemProperties().dleEditorHead();
+        String scriptInjectionRuntimePropertiesDleEditorHead = ScriptInjectionPropertiesProvider.getScripInjectionRuntimeProperties().dleEditorHead();
 
-        return getConfigurationStatus(DLE_TOOLBAR, content, "scriptInjection.dleEditorHead=<script src=\"/polarion/pdf-exporter/js/starter.js\"></script>.*<script>PdfExporterStarter.injectToolbar(.*);</script>");
+        ConfigurationStatus configurationStatusSystemProperties = getConfigurationStatus(DLE_TOOLBAR, scriptInjectionSystemPropertiesDleEditorHead, DLE_TOOLBAR_SCRIPT_REGEX);
+        ConfigurationStatus configurationStatusRuntimeProperties = getConfigurationStatus(DLE_TOOLBAR, scriptInjectionRuntimePropertiesDleEditorHead, DLE_TOOLBAR_SCRIPT_REGEX);
+
+        if (configurationStatusSystemProperties.getStatus() == Status.OK) {
+            return configurationStatusSystemProperties;
+        } else {
+            return configurationStatusRuntimeProperties;
+        }
     }
 }
