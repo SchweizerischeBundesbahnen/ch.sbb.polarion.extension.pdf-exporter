30a3840e9668f9351b8b85bec147e9e6578210ba
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-02-12 18:10:25 +0100

    fix: show Polarion version in About page (#374)
    Refs: #369

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/VersionUtils.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/VersionUtils.java
@@ -1,6 +1,5 @@
 package ch.sbb.polarion.extension.pdf_exporter.util;
 
-import ch.sbb.polarion.extension.pdf_exporter.util.configuration.WeasyPrintStatusProvider;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.Nullable;
 
@@ -10,19 +9,27 @@ import java.util.Properties;
 
 @UtilityClass
 public class VersionUtils {
+    private static final String VERSION_FILE = "versions.properties";
 
     public static @Nullable String getLatestCompatibleVersionWeasyPrintService() {
-        try (InputStream input = WeasyPrintStatusProvider.class.getClassLoader().getResourceAsStream("versions.properties")) {
+        return getVersionProperty("weasyprint-service.version");
+    }
+
+    public static @Nullable String getCurrentCompatibleVersionPolarion() {
+        return getVersionProperty("polarion.version");
+    }
+
+    private static @Nullable String getVersionProperty(String key) {
+        try (InputStream input = VersionUtils.class.getClassLoader().getResourceAsStream(VERSION_FILE)) {
             if (input == null) {
                 return null;
             }
 
             Properties properties = new Properties();
             properties.load(input);
-            return properties.getProperty("weasyprint-service.version");
+            return properties.getProperty(key);
         } catch (IOException e) {
             return null;
         }
     }
-
 }
---
+++ src/test/java/ch/sbb/polarion/extension/pdf_exporter/util/configuration/PolarionStatusProviderTest.java
@@ -0,0 +1,58 @@
+package ch.sbb.polarion.extension.pdf_exporter.util.configuration;
+
+import ch.sbb.polarion.extension.generic.configuration.ConfigurationStatus;
+import ch.sbb.polarion.extension.generic.configuration.ConfigurationStatusProvider;
+import ch.sbb.polarion.extension.generic.configuration.Status;
+import ch.sbb.polarion.extension.pdf_exporter.util.VersionUtils;
+import com.polarion.core.config.Configuration;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
+
+class PolarionStatusProviderTest {
+
+
+    public static Stream<Arguments> provideConfigurationStatus() {
+        return Stream.of(
+                Arguments.of("2410", "2410", configurationStatus(Status.OK, "2410")),
+                Arguments.of("2410.1", "2410", configurationStatus(Status.OK, "2410.1")),
+                Arguments.of("2310", "2410", configurationStatus(Status.WARNING, "2310 is not official supported")),
+                Arguments.of("2310.1", "2410", configurationStatus(Status.WARNING, "2310.1 is not official supported")),
+                Arguments.of("2410", "", configurationStatus(Status.ERROR, "Official supported version not set"))
+        );
+    }
+
+    private static ConfigurationStatus configurationStatus(Status status, String details) {
+        return ConfigurationStatus.builder()
+                .name(PolarionStatusProvider.POLARION_ALM)
+                .status(status)
+                .details(details)
+                .build();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideConfigurationStatus")
+    void testConfigurationStatus(String polarionVersion, String currentCompatibleVersionPolarion, ConfigurationStatus expectedConfigurationStatus) {
+        PolarionStatusProvider polarionStatusProvider = new PolarionStatusProvider();
+
+        try (
+                MockedStatic<Configuration> configurationMockedStatic = mockStatic(Configuration.class, Mockito.RETURNS_DEEP_STUBS);
+                MockedStatic<VersionUtils> versionUtilsMockedStatic = mockStatic(VersionUtils.class)
+        ) {
+            configurationMockedStatic.when(() -> Configuration.getInstance().getProduct().versionName()).thenReturn(polarionVersion);
+            versionUtilsMockedStatic.when(VersionUtils::getCurrentCompatibleVersionPolarion).thenReturn(currentCompatibleVersionPolarion);
+
+            ConfigurationStatusProvider.Context context = ConfigurationStatusProvider.Context.builder().scope("").build();
+            ConfigurationStatus configurationStatus = polarionStatusProvider.getStatus(context);
+            assertEquals(expectedConfigurationStatus, configurationStatus);
+        }
+    }
+
+}
