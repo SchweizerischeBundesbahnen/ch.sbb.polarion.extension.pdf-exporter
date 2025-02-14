aa91ba6baff7a7bf0347c1b8251ec5a93f8dce98
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-02-14 19:22:38 +0100

    fix: Table of Content for LiveReports/TestRuns
    Refs: #378

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/HtmlProcessor.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/HtmlProcessor.java
@@ -26,7 +26,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static ch.sbb.polarion.extension.pdf_exporter.util.exporter.Constants.*;
 
