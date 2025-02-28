7d950f8f2eddec8081d2386447a5955804531a19
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-02-28 18:49:21 +0100

    feat: "fit to page" option for /convert/html endpoint
    Refs: #376

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/adjuster/ImageSizeAdjuster.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/adjuster/ImageSizeAdjuster.java
@@ -1,14 +1,18 @@
 package ch.sbb.polarion.extension.pdf_exporter.util.adjuster;
 
-import ch.sbb.polarion.extension.generic.regex.RegexMatcher;
 import ch.sbb.polarion.extension.pdf_exporter.rest.model.conversion.ConversionParams;
 import ch.sbb.polarion.extension.pdf_exporter.util.HtmlProcessor;
 import ch.sbb.polarion.extension.pdf_exporter.util.PaperSizeConstants;
+import com.steadystate.css.dom.CSSStyleDeclarationImpl;
+import com.steadystate.css.parser.CSSOMParser;
 import org.jetbrains.annotations.NotNull;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.w3c.css.sac.InputSource;
+import org.w3c.dom.css.CSSStyleDeclaration;
 
+import java.io.StringReader;
 import java.util.Optional;
 
 public class ImageSizeAdjuster extends AbstractAdjuster {
@@ -19,14 +23,10 @@ public class ImageSizeAdjuster extends AbstractAdjuster {
 
     @Override
     public void execute() {
-
-        // get max width and height depending on page orientation
         float maxWidth = PaperSizeConstants.getMaxWidth(conversionParams);
         float maxHeight = PaperSizeConstants.getMaxHeight(conversionParams);
 
-        // We are looking here for images which widths and heights are explicitly specified.
-        // Then we check if width exceeds limit we override it by value "100%"
-        Elements images = document.select("img[style]"); // getting <img> with style attribute
+        Elements images = document.select("img[style]");
 
         for (Element img : images) {
             adjustImageSize(img, maxWidth, maxHeight);
@@ -35,48 +35,73 @@ public class ImageSizeAdjuster extends AbstractAdjuster {
 
     private void adjustImageSize(@NotNull Element img, float maxWidth, float maxHeight) {
         String style = img.attr("style");
+        CSSStyleDeclaration cssStyle = parseCss(style);
 
-        float width = extractDimension(style, "width");
-        float height = extractDimension(style, "height");
+        float width = extractDimension(cssStyle, "width");
+        float mWidth = extractDimension(cssStyle, "max-width");
+        float height = extractDimension(cssStyle, "height");
 
         float widthExceedingRatio = width / maxWidth;
+        float maxWidthExceedingRatio = mWidth / maxWidth;
         float heightExceedingRatio = height / maxHeight;
 
-        if (widthExceedingRatio <= 1 && heightExceedingRatio <= 1) {
+        if (widthExceedingRatio <= 1 && heightExceedingRatio <= 1 && maxWidthExceedingRatio <= 1) {
             return;
         }
 
-        final float adjustedWidth;
-        final float adjustedHeight;
+        float adjustedWidth = 0;
+        float adjustedMaxWidth = 0;
+        float adjustedHeight = 0;
 
         if (widthExceedingRatio > heightExceedingRatio) {
             adjustedWidth = width / widthExceedingRatio;
             adjustedHeight = height / widthExceedingRatio;
+        } else if (maxWidthExceedingRatio > heightExceedingRatio) {
+            adjustedMaxWidth = mWidth / maxWidthExceedingRatio;
+            adjustedHeight = height / maxWidthExceedingRatio;
         } else {
+            adjustedMaxWidth = mWidth / heightExceedingRatio;
             adjustedWidth = width / heightExceedingRatio;
             adjustedHeight = height / heightExceedingRatio;
         }
 
-        style = style.replaceAll("width:\\s*[^;]+;", String.format("width: %dpx;", (int) adjustedWidth)).trim();
-        style = style.replaceAll("height:\\s*[^;]+;", String.format("height: %dpx;", (int) adjustedHeight)).trim();
+        if (adjustedWidth > 0) {
+            cssStyle.setProperty("width", (int) adjustedWidth + "px", "");
+        }
+        if (adjustedMaxWidth > 0) {
+            cssStyle.setProperty("max-width", (int) adjustedMaxWidth + "px", "");
+        }
+        if (adjustedHeight > 0) {
+            cssStyle.setProperty("height", (int) adjustedHeight + "px", "");
+        }
 
-        img.attr("style", style);
+        img.attr("style", cssStyle.getCssText());
     }
 
-    private float extractDimension(String style, String property) {
-        String regex = property + ":\\s*([\\d.]+)(px|ex)?";
-        String result = RegexMatcher.get(regex, RegexMatcher.DOTALL | RegexMatcher.CASE_INSENSITIVE)
-                .findFirst(style, regexEngine -> {
-                    String value = regexEngine.group(1);
-                    String unit = Optional.ofNullable(regexEngine.group(2)).orElse("");
-                    return value + unit;
-                })
-                .orElse("0");
+    private float extractDimension(CSSStyleDeclaration cssStyle, String property) {
+        String value = Optional.ofNullable(cssStyle.getPropertyValue(property)).orElse("").trim();
 
-        if (result.endsWith("ex")) {
-            return Float.parseFloat(result.replace("ex", "")) * HtmlProcessor.EX_TO_PX_RATIO;
+        if (value.isEmpty()) {
+            return 0;
+        }
+
+        if (value.endsWith("ex")) {
+            return Float.parseFloat(value.replace("ex", "")) * HtmlProcessor.EX_TO_PX_RATIO;
+        } else if (value.endsWith("px")) {
+            return Float.parseFloat(value.replace("px", ""));
+        } else if (value.endsWith("%")) {
+            return 0;
         } else {
-            return Float.parseFloat(result.replace("px", ""));
+            return 0;
+        }
+    }
+
+    private CSSStyleDeclaration parseCss(String style) {
+        CSSOMParser parser = new CSSOMParser();
+        try {
+            return parser.parseStyleDeclaration(new InputSource(new StringReader(style)));
+        } catch (Exception e) {
+            return new CSSStyleDeclarationImpl();
         }
     }
 }
