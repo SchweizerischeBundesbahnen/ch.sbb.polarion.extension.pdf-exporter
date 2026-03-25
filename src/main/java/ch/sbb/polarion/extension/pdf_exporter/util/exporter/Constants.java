749b48ec498c72c0a86cd2273d4f6353a723f9a6
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2026-03-25 13:01:46 +0100

    fix: convert Polarion `<div class="heading-N">` tags to `<h6>` for export compatibility (#771)
    - Added logic to handle `div.heading-N` elements beyond `<h6>` (levels 7-30) in HTML processor.
    - Updated tests to validate new `<div>` to `<h6>` conversions.
    - Introduced `H_TAG_MAX_LEVEL` constant and updated related documentation.
    - Minor adjustments in `package-lock.json` to include `peer` attribute.
    
    Refs: #770

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/exporter/Constants.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/exporter/Constants.java
@@ -7,6 +7,12 @@ public final class Constants {
      */
     public static final int H_TAG_MIN_PRIORITY = 6;
 
+    /**
+     * Maximum heading level supported by Polarion.
+     * Levels 1-6 use standard &lt;h1&gt;-&lt;h6&gt; tags, levels 7-30 use &lt;div class="heading-N"&gt;.
+     */
+    public static final int H_TAG_MAX_LEVEL = 30;
+
     public static final String PAGE_BREAK = "PAGE_BREAK";
     public static final String PORTRAIT_ABOVE = "PORTRAIT_ABOVE";
     public static final String LANDSCAPE_ABOVE = "LANDSCAPE_ABOVE";
