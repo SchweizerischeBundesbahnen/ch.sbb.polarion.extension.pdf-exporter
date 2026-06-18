d6c1e1e09b224cabb60e11d31d06e8ecbe278305
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2026-06-18 16:39:06 +0200

    refactor: remove unused DocumentBuilder import in PdfA4Processor
    Refs: #840

--- src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/PdfA4Processor.java
+++ src/main/java/ch/sbb/polarion/extension/pdf_exporter/util/PdfA4Processor.java
@@ -18,7 +18,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.ByteArrayOutputStream;
